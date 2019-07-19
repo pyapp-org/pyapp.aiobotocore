67c836dfe533f1cf9e0c5696bfbdf4d81e02de21
Tim Savage <tim@savage.company>
2019-07-20 01:07:19 +1000

    Migrated to using __version__ file

--- pyapp_ext/aiobotocore/__init__.py
+++ pyapp_ext/aiobotocore/__init__.py
@@ -1,8 +1,11 @@
 """
-pyApp - AIOBotocore Extension
+pyApp - AIO Botocore Extension
 
 """
+from .__version__ import __version__
 from .factory import *
+
+version_info = tuple(int(v) for v in __version__.split("."))
 
 
 class Extension:
