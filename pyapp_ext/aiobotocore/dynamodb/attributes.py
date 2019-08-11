4d58a1056f8218bcf64c726bfda2ad905272d24b
Tim Savage <tim@savage.company>
2019-08-11 20:04:44 +1000

    Added set and list attributes, removed python_type (not required)

--- pyapp_ext/aiobotocore/dynamodb/attributes.py
+++ pyapp_ext/aiobotocore/dynamodb/attributes.py
@@ -1,5 +1,7 @@
-import uuid
+from typing import List, Set, Any
+
 import datetime
+import uuid
 import yarl
 
 from .base import Attribute, DataType
@@ -9,19 +11,19 @@ class StringAttribute(Attribute[str]):
     """
     String attribute
     """
-    python_type = str
+
     dynamo_type = DataType.String
 
     def prepare(self, value: str) -> str:
         return str(value)
 
 
-class BytesAttribute(Attribute[bytes]):
+class BinaryAttribute(Attribute[bytes]):
     """
-    String attribute
+    Binary attribute
     """
-    python_type = bytes
-    dynamo_type = DataType.Bytes
+
+    dynamo_type = DataType.Binary
 
     def prepare(self, value: bytes) -> str:
         return value.hex()
@@ -31,7 +33,7 @@ class IntegerAttribute(Attribute[int]):
     """
     Integer attribute
     """
-    python_type = int
+
     dynamo_type = DataType.Number
 
     def prepare(self, value: int) -> str:
@@ -42,7 +44,7 @@ class FloatAttribute(Attribute[float]):
     """
     Float attribute
     """
-    python_type = float
+
     dynamo_type = DataType.Number
 
     def prepare(self, value: float) -> str:
@@ -53,7 +55,7 @@ class BooleanAttribute(Attribute[bool]):
     """
     Boolean Attribute
     """
-    python_type = bool
+
     dynamo_type = DataType.Bool
 
 
@@ -61,7 +63,7 @@ class DateTimeAttribute(Attribute[datetime.datetime]):
     """
     Date/Time attribute
     """
-    python_type = datetime.datetime
+
     dynamo_type = DataType.String
 
     def prepare(self, value: datetime.datetime) -> str:
@@ -72,7 +74,7 @@ class UUIDAttribute(Attribute[uuid.UUID]):
     """
     UUID attribute
     """
-    python_type = uuid.UUID
+
     dynamo_type = DataType.String
 
     def prepare(self, value: uuid.UUID) -> str:
@@ -83,20 +85,91 @@ class URLAttribute(Attribute[yarl.URL]):
     """
     URL attribute
     """
-    python_type = yarl.URL
+
     dynamo_type = DataType.String
 
     def prepare(self, value: yarl.URL) -> str:
         return str(value)
 
 
+class StringSetAttribute(StringAttribute):
+    """
+    String Set Attribute
+    """
+
+    dynamo_type = DataType.StringSet
+
+    def prepare(self, value: Set[str]) -> List[str]:
+        super_prepare = super().prepare
+        return list(super_prepare(v) for v in value)
+
+
+class BinarySetAttribute(BinaryAttribute):
+    """
+    Binary Set Attribute
+    """
+
+    dynamo_type = DataType.BinarySet
+
+    def prepare(self, value: Set[bytes]) -> List[str]:
+        super_prepare = super().prepare
+        return list(super_prepare(v) for v in value)
+
+
+class IntegerSetAttribute(IntegerAttribute):
+    """
+    Integer Set Attribute
+    """
+
+    dynamo_type = DataType.NumberSet
+
+    def prepare(self, value: Set[int]) -> List[str]:
+        super_prepare = super().prepare
+        return list(super_prepare(v) for v in value)
+
+
+class FloatSetAttribute(FloatAttribute):
+    """
+    Float Set Attribute
+    """
+
+    dynamo_type = DataType.NumberSet
+
+    def prepare(self, value: Set[float]) -> List[str]:
+        super_prepare = super().prepare
+        return list(super_prepare(v) for v in value)
+
+
+class ListAttribute(Attribute):
+    """
+    List Attribute
+    """
+
+    dynamo_type = DataType.List
+
+    def __init__(self, containing_attribute: Attribute, name: str = None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.containing_attribute = containing_attribute
+
+    def prepare(self, value: List[Any]) -> List[Any]:
+        containing_prepare = self.containing_attribute.prepare
+        return list(containing_prepare(v) for v in value)
+
+
 SIMPLE_TYPES = {
     str: StringAttribute,
-    bytes: BytesAttribute,
+    bytes: BinaryAttribute,
     bool: BooleanAttribute,
     int: IntegerAttribute,
     float: FloatAttribute,
     datetime.datetime: DateTimeAttribute,
     uuid.UUID: UUIDAttribute,
-    yarl.URL: URLAttribute
+    yarl.URL: URLAttribute,
+}
+
+SET_TYPES = {
+    str: StringSetAttribute,
+    bytes: BinarySetAttribute,
+    int: IntegerSetAttribute,
+    float: FloatSetAttribute,
 }
